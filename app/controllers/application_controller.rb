c4dd589f3531b0d211e1239033a289b8e76b8d73
nakanowatari-taiki <starrito@icloud.com>
2021-04-10 17:13:36 +0900

    4/10コミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
-
+  before_action :basic_auth
+  
+  private
   def configre_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
@@ -8,4 +10,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource) 
     plans_path
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
+    end
+  end
+
 end
